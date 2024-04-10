d5f46be11a11cf8c25bfa3f7eef194d23b38da73
Andreas Pramendorfer <1praand1@gmail.com>
2024-04-10 14:22:16 +0200

    finish him

---
+++ main.go
@@ -0,0 +1,18 @@
+// main.go
+
+package main
+
+
+func main() {
+	a := App{}
+	a.Initialize("postgres",
+		"",
+		"postgres")
+
+	//export APP_DB_USERNAME=postgres
+	//export APP_DB_PASSWORD=<whatever password you use>
+	//export APP_DB_NAME=postgres
+
+	//List env variables (bash): printenv
+	a.Run(":8010")
+}
